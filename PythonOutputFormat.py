ce057d5b7a2443d8061f87264a1454033a1a9457
Jeppe Toustrup <jeppe@tenzer.dk>
2015-03-07 18:19:40 +0100

    A bunch of improvements:
    - The indentation is now based on the settings of the document instead of being hardcoded to 4 spaces
    - Added support for selecting one or more regions of a document and format them individually
    - Changes the syntax setting of the document to Python if it's set to Plain Text
    - Made script PEP257 compliant (docstrings)
    - Added some PEP8 and Pylint recommendations

--- PythonOutputFormat.py
+++ PythonOutputFormat.py
@@ -1,44 +1,72 @@
-import sublime, sublime_plugin
+"""Sublime Text plugin to format the output of Python programs nicely."""
+
 import tokenize
 from io import BytesIO
 
+import sublime
+import sublime_plugin
+
 
 class PythonOutputFormatCommand(sublime_plugin.TextCommand):
+
+    """The Sublime Text plugin class."""
+
     indentation_level = 0
+    newline = (tokenize.NL, '\n')
+    indentation = '    '
+
+    def set_indentation(self):
+        """Set internal variable based on the indentation settings of the document."""
+        if self.view.settings_object.get('translate_tabs_to_spaces'):
+            self.indentation = ' ' * self.view.settings_object.get('tab_size')
+        else:
+            self.indentation = '\t'
 
     def indent(self):
-        return (tokenize.INDENT, ' ' * 4 * self.indentation_level)
+        """Return the indentation needed at the current level."""
+        return (tokenize.INDENT, self.indentation * self.indentation_level)
 
     def run(self, edit):
-        NEWLINE = (tokenize.NL, '\n')
-        result = []
-        entire_document = sublime.Region(0, self.view.size())
-        data_input = tokenize.tokenize(BytesIO(self.view.substr(entire_document).encode('utf-8')).readline)
+        """Main function which is called when the plugin is activated."""
+        self.set_indentation()
 
-        for token in data_input:
-            if token.type == tokenize.OP:
-                if token.exact_type in [tokenize.LBRACE, tokenize.LSQB]:
-                    result.append(self.indent())
-                    result.append((token.exact_type, token.string))
-                    result.append(NEWLINE)
-                    self.indentation_level += 1
-                elif token.exact_type in [tokenize.RBRACE, tokenize.RSQB]:
-                    if result[-1] != NEWLINE:
-                        result.append(NEWLINE)
-                    self.indentation_level -= 1
-                    result.append(self.indent())
-                    result.append((token.exact_type, token.string))
-                elif token.exact_type == tokenize.COLON:
-                    result.append((token.exact_type, token.string))
-                    result.append((tokenize.STRING, ' '))
-                elif token.exact_type == tokenize.COMMA:
-                    result.append((token.exact_type, token.string))
-                    result.append(NEWLINE)
-                else:
-                    result.append((token.exact_type, token.string))
-            elif token.type != tokenize.NL:
-                result.append(self.indent())
-                result.append((token.exact_type, token.string))
+        for region in self.view.sel():
+            if region.empty():
+                region = sublime.Region(0, self.view.size())
 
-        result.append(NEWLINE)
-        self.view.replace(edit, entire_document, tokenize.untokenize(result).decode('utf-8'))
+            region_data = self.view.substr(region).encode('utf-8')
+            result = []
+
+            try:
+                for token in tokenize.tokenize(BytesIO(region_data).readline):
+                    if token.type == tokenize.OP:
+                        if token.exact_type in [tokenize.LBRACE, tokenize.LSQB]:
+                            result.append(self.indent())
+                            result.append((token.exact_type, token.string))
+                            result.append(self.newline)
+                            self.indentation_level += 1
+                        elif token.exact_type in [tokenize.RBRACE, tokenize.RSQB]:
+                            if result[-1] != self.newline:
+                                result.append(self.newline)
+                            self.indentation_level -= 1
+                            result.append(self.indent())
+                            result.append((token.exact_type, token.string))
+                        elif token.exact_type == tokenize.COLON:
+                            result.append((token.exact_type, token.string))
+                            result.append((tokenize.STRING, ' '))
+                        elif token.exact_type == tokenize.COMMA:
+                            result.append((token.exact_type, token.string))
+                            result.append(self.newline)
+                        else:
+                            result.append((token.exact_type, token.string))
+                    elif token.type != tokenize.NL:
+                        result.append(self.indent())
+                        result.append((token.exact_type, token.string))
+            except tokenize.TokenError as error:
+                sublime.status_message(str(error))
+                return
+
+            self.view.replace(edit, region, tokenize.untokenize(result).decode('utf-8'))
+
+        if self.view.settings().get('syntax') == 'Packages/Text/Plain text.tmLanguage':
+            self.view.set_syntax_file('Packages/Python/Python.tmLanguage')
